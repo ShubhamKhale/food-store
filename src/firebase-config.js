95fd3eab031790decacb05659232dfeb52c7e905
Shubham <shubhamkhale301@gmail.com>
2023-07-11 02:13:07 +0530

    added pages

---
+++ src/firebase-config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAg7lHbW4O-EskhX7b-rkjjAlT4qlYFqC0",
+  authDomain: "food-store-f4454.firebaseapp.com",
+  projectId: "food-store-f4454",
+  storageBucket: "food-store-f4454.appspot.com",
+  messagingSenderId: "472137751875",
+  appId: "1:472137751875:web:59a0034943d28da155e3f6"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
