ef611c8a72389cf678ac7663c3f68aece0b2e06e
Shubham <shubhamkhale301@gmail.com>
2023-07-10 23:50:46 +0530

    added banner.jsx element

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,6 +1,10 @@
+import { Banner } from "../../components/Banner";
+
 const Home = () => {
     return (
-        <div className="text-white">Home</div>
+        <>
+            <Banner />
+        </>
     )
 }  
 
